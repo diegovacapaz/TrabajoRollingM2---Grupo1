d1cc38bf05dc33fadc9b4b70be48e8d4e78ac35b
Walter Conticello <walterconticellomsmt@gmail.com>
2023-04-27 14:03:58 -0300

    layout general de la pagina, opcion 1

---
+++ sliderScript.js
@@ -0,0 +1,14 @@
+let swiper = new Swiper(".miSwiper", {
+	effect: "coverflow",
+	grabCursor: true,
+	centeredSlides: true,
+	slidesPerView: "auto",
+	coverflowEffect: {
+		rotate: 15,
+		stretch: 0,
+		depth: 300,
+		modifier: 1,
+		slideShadows: true,
+	},
+	loop: true,
+});
