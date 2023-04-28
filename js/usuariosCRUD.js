9e137b9a9b34cdb33efd4f75a8f4403215e41483
Diego Vaca <diegovacapaz@hotmail.com>
2023-04-27 22:06:46 -0300

    Se realizó la funcion registro

--- js/usuariosCRUD.js
+++ js/usuariosCRUD.js
@@ -27,3 +27,39 @@ class Cuenta{
         this.clave = clave;
     }
 }
+
+// CRUD USUARIOS
+let cuentas = [];
+const cuentaAdmin = new Cuenta("admin","admin@admin.com","1234",true);
+cuentas.push(cuentaAdmin);
+
+// ALTA USUARIOS
+const registro = (usuario,correo,clave) => {
+    if(usuario === null || usuario === undefined || clave === null || clave === undefined || correo === null || correo === undefined){
+        return null;
+    }
+    let usuarioRepetido = false;
+    let correoRepetido = false;
+    for(let i=0; i<cuentas.length; i++){
+        if(cuentas[i].usuario === usuario){
+            usuarioRepetido = true;
+        }
+        if(cuentas[i].correo === correo){
+            correoRepetido = true;
+        }
+    }
+    if(usuarioRepetido && correoRepetido){
+        return "El nombre de usuario y el correo ya estan usados";
+    }
+    if(usuarioRepetido){
+        return "El nombre de usuario ya esta usado";
+    }
+    if(correoRepetido){
+        return "El correo ya esta usado";
+    }
+    let nuevoUsuario = new Cuenta(usuario, correo, clave, false);
+    cuentas.push(nuevoUsuario);
+    return "Se ha registrado con éxito";
+}
+// MODIFICAR LISTA
+// ELIMINAR USUARIOS
