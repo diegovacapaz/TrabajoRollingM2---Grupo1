8faf4bcec4de588e9c7104137204ae6fd554d176
Leonardo Aguilera Castro <leoac92@gmail.com>
2023-05-02 23:37:25 -0300

    subiendo CrudProductos

---
+++ Juegos-form.js
@@ -0,0 +1,83 @@
+let tablaJuego = localStorage.getItem("tablaJuegoStorage");
+tablaJuego = JSON.parse(tablaJuego);
+if (tablaJuego == null) {
+    let tablaJuego = [];
+}
+
+
+let idForm = localStorage.getItem("idForm");
+idForm = JSON.parse(idForm);
+if (idForm == null) {
+    let idForm = 0;
+}
+
+cargarPagina();
+
+function guardar() {
+
+
+    Swal.fire({
+        title: 'GUARDAR',
+        html: 'DESEA GUARDAR LOS CAMBIOS?',
+        showDenyButton: true,
+        confirmButtonText: 'SI',
+        denyButtonText: 'NO'
+    }).then(
+        (result) => {
+            if (result.isConfirmed) {
+
+                console.log("PRESIONO GUARDAR...");
+                let objJuego = JSON.stringify({
+                    idJuego: (idForm > 0) ? idForm : (tablaJuego.length + 1),
+                    Nombre: document.getElementById("txtNombre").value,
+                    genero: document.getElementById("cboGenero").value,
+                    precio: document.getElementById("txtPrecio").value,
+                    
+                });
+                console.log(objJuego);
+                //EDITAR
+                if (idForm > 0) {
+                    for (const i in tablaJuego) {
+                        let varJuego = JSON.parse(tablaJuego[i]);
+                        if (varJuego.idJuego == idForm) {
+                            tablaJuego[i] = objJuego;
+                            break;
+                        }
+
+                    }
+
+                } else {
+                    // NUEVOS Juegos
+                    tablaJuego.push(objJuego);
+                }
+
+                localStorage.setItem("tablaJuegoStorage", JSON.stringify(tablaJuego));
+
+                Swal.fire('CAMBIOS  GUARDADOS','','success').then(
+                    (result)=>{
+                        window.location.replace("Juegos.html");
+                    }
+                );
+            }else if (result.isDenied){
+                Swal.fire('CAMBIOS NO GUARDADOS','','info');
+            }
+        }
+    );
+
+}
+
+function cargarPagina() {
+    if (idForm > 0) {
+        // SACAR DATOS DE LA FILA DE LA TABLA Y PONERLO EN EL FORMULARIO
+        for (const i in tablaJuego) {
+            let varJuego = JSON.parse(tablaJuego[i]);
+            if (varJuego.idJuego == idForm) {
+                document.getElementById("txtIdJuego").value = varJuego.idJuego;
+                document.getElementById("txtNombre").value = varJuego.nombre;
+                document.getElementById("cboGenero").value = varJuego.genero;
+                document.getElementById("txtPrecio").value = varJuego.precio;
+                break;
+            }
+        }
+    }
+}
---
+++ Juegos.js
@@ -0,0 +1,51 @@
+let tablaJuego = localStorage.getItem("tablaJuegoStorage");
+if (tablaJuego !== null) {
+    tablaJuego = JSON.parse(tablaJuego);
+} else {
+    tablaJuego = [];
+}
+
+listar();
+
+function listar() {
+    console.log("INGRESANDO A LISTAR...");
+
+    var dataFila = '';
+
+    if(tablaJuego.length > 0){
+        for(const i in tablaJuego){
+            let varJuego = JSON.parse(tablaJuego[i]);
+            dataFila += "<tr>";
+            dataFila += "<td>"+varJuego.idJuego+"</td>";
+            dataFila += "<td>"+varJuego.Nombre+"</td>";
+            dataFila += "<td>"+varJuego.genero+"</td>";
+            dataFila += "<td>"+varJuego.precio+"</td>";
+            dataFila += "<td>"+
+                        "<button type='button' class='btn btn-warning' onclick='abrirForm("+varJuego.idJuego+")'>EDITAR</button>"+
+                        "<button type='button' class='btn btn-info' onclick='eliminarItem("+varJuego.idJuego+")'>ELIMINAR</button>"+
+                        "</td>";
+            dataFila += "</tr>";
+
+        }
+        document.getElementById("dataJuego").innerHTML = dataFila;
+    }
+    else{
+        document.getElementById("dataJuego").innerHTML = "<tr><td colspan='7'>No hay Juegos</td></tr>";
+    }
+}
+
+function abrirForm(idForm){
+    localStorage.setItem("idForm", JSON.stringify(idForm));
+    window.location.replace("Juegos-form.html");
+}
+
+function eliminarItem(idItem){
+    for(const i in tablaJuego){
+        let varJuego = JSON.parse(tablaJuego[i]);
+        if(varJuego.idJuego == idItem){
+            tablaJuego.splice(i,1);
+            localStorage.setItem("tablaJuegoStorage", JSON.stringify(tablaJuego));
+        }
+    }
+    listar()
+}
