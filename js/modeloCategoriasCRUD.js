8ebcc8a0e0758395e738bc8d450a196197720afc
Diego Vaca Paz <diegovacapaz@gmail.com>
2023-05-05 20:36:23 -0300

    Se realizo la funcion borrarCategoria

--- js/modeloCategoriasCRUD.js
+++ js/modeloCategoriasCRUD.js
@@ -6,11 +6,22 @@ const crearCategoria = (categoria) => {
     categoria = categoria.toLowerCase();
     if(!categorias.includes(categoria)){
         categorias.push(categoria);
-        return "categoria creada con éxito";
+        return "Categoria creada con éxito";
     }
     return "La categoría ya existe";
 }
 //Eliminar categorias
 const borrarCategoria = (categoria) => {
-
+    categoria = categoria.toLowerCase();
+    if(categorias.includes(categoria)){
+        categorias.splice(categorias.indexOf(categoria), 1);
+        return "Categoria borrada con éxito";
+    }
+    return "La categoría no existe";
 }
+
+console.log(categorias);
+console.log(crearCategoria("Terror"));
+console.log(crearCategoria("Aventura"));
+console.log(crearCategoria("terror"));
+console.log(categorias);
