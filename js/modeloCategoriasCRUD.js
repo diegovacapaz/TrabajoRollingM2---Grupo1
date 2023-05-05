4210836ca4f4eea79b65faea72fa8b566108af6f
Diego Vaca Paz <diegovacapaz@gmail.com>
2023-05-05 20:38:57 -0300

    Se añadio trim() a la entrada de las funciones

--- js/modeloCategoriasCRUD.js
+++ js/modeloCategoriasCRUD.js
@@ -3,7 +3,7 @@ let categorias = [];
 
 //Crear categorias
 const crearCategoria = (categoria) => {
-    categoria = categoria.toLowerCase();
+    categoria = categoria.toLowerCase().trim();
     if(!categorias.includes(categoria)){
         categorias.push(categoria);
         return "Categoria creada con éxito";
@@ -12,7 +12,7 @@ const crearCategoria = (categoria) => {
 }
 //Eliminar categorias
 const borrarCategoria = (categoria) => {
-    categoria = categoria.toLowerCase();
+    categoria = categoria.toLowerCase().trim();
     if(categorias.includes(categoria)){
         categorias.splice(categorias.indexOf(categoria), 1);
         return "Categoria borrada con éxito";
@@ -20,8 +20,13 @@ const borrarCategoria = (categoria) => {
     return "La categoría no existe";
 }
 
+//Testing
 console.log(categorias);
 console.log(crearCategoria("Terror"));
+console.log(crearCategoria("Terror  "));
 console.log(crearCategoria("Aventura"));
 console.log(crearCategoria("terror"));
+console.log(categorias);
+console.log(borrarCategoria("Drama"));
+console.log(borrarCategoria("   aventura "));
 console.log(categorias);
