b2c292ada36a452608f73a9dcc3e6963fbbb5bb4
Diego Vaca Paz <diegovacapaz@gmail.com>
2023-05-05 19:10:38 -0300

    Se comento las funciones deseadas

--- js/controladorBotonLogOutIn.js
+++ js/controladorBotonLogOutIn.js
@@ -1,14 +1,20 @@
 //IMPORTACIÓN DEL MODELO
 import {cuentas, cuentaActiva, registro, buscarCuentaCorreo, buscarCuentaUsuario, iniciarSesion, eliminarCuenta, cerrarSesion} from "./modeloUsuariosCRUD.js";
 
-const botonLoginLogout = getElementById("LogInOut_Boton");
+const botonLog = document.getElementById("botonLog");
+const botonLogDivTexto = document.getElementById("botonLogTexto");
 
-botonLoginLogout.addEventListener("click",()=>{
-    if(cuentaActiva !== null){
-        
-    }
-    else{
-        // window.location.replace(); otra opcion
-        window.location.href = "../pages/LoginRegistro.html";
-    }
-});
+
+/*Revisar estado de cuentaActiva
+
+Si hay una cuenta logueada,
+mostrar su nombre de usuario y agregar un boton de logout
+el usuario lleva a error404
+el logo lleva a error404
+el logout cierra sesion y recarga la pagina
+
+Si no hay cuenta logueada,
+mostrar ingresar
+el logo lleva a LoginRegistro.html
+el ingresar lleva a LoginRegistro.html
+*/
