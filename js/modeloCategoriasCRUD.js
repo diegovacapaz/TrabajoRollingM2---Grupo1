a712ff73793eb81d6de4b6372e58e83735d715c4
Diego Vaca Paz <diegovacapaz@gmail.com>
2023-05-06 20:02:45 -0300

    Se añadio el local storage para las funciones crear y borrar cateoria

--- js/modeloCategoriasCRUD.js
+++ js/modeloCategoriasCRUD.js
@@ -16,6 +16,7 @@ const crearCategoria = (categoria) => {
     categoria = categoria.toLowerCase().trim();
     if(!categorias.includes(categoria)){
         categorias.push(categoria);
+        localStorage.setItem(lsCategoriasId,JSON.stringify(categorias));
         return "Categoria creada con éxito";
     }
     return "La categoría ya existe";
@@ -25,21 +26,22 @@ const borrarCategoria = (categoria) => {
     categoria = categoria.toLowerCase().trim();
     if(categorias.includes(categoria)){
         categorias.splice(categorias.indexOf(categoria), 1);
+        localStorage.setItem(lsCategoriasId,JSON.stringify(categorias));
         return "Categoria borrada con éxito";
     }
     return "La categoría no existe";
 }
 
 //Testing
-console.log(categorias);
-console.log(crearCategoria("Terror"));
-console.log(crearCategoria("Terror  "));
-console.log(crearCategoria("Aventura"));
-console.log(crearCategoria("terror"));
-console.log(categorias);
-console.log(borrarCategoria("Drama"));
-console.log(borrarCategoria("   aventura "));
-console.log(categorias);
+// console.log(categorias);
+// console.log(crearCategoria("Terror"));
+// console.log(crearCategoria("Terror  "));
+// console.log(crearCategoria("Aventura"));
+// console.log(crearCategoria("terror"));
+// console.log(categorias);
+// console.log(borrarCategoria("Drama"));
+// console.log(borrarCategoria("   aventura "));
+// console.log(categorias);
 
 //EXPORTACIÓN
-//export {categorias, crearCategoria, borrarCategoria};
+//export {categorias, crearCategoria, borrarCategoria}; ESTA LINEA SE DEBE DESCOMENTAR LUEGO DE TESTEAR, PARA PODER UTILIZARLO COMO MODULO
