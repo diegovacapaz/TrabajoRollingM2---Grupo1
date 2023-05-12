87cd8950091b7f06feabf4752450285e6bf8ec5e
Diego Vaca Paz <diegovacapaz@gmail.com>
2023-05-12 17:01:37 -0300

    Se creo el archivo de los permisos de administracion

---
+++ js/permisosAdmin.js
@@ -0,0 +1,2 @@
+//IMPORTACIÓN DEL MODELO
+import {cuentas, cuentaActiva, registro, buscarCuentaCorreo, buscarCuentaUsuario, iniciarSesion, eliminarCuenta, cerrarSesion} from "./modeloUsuariosCRUD.js";
