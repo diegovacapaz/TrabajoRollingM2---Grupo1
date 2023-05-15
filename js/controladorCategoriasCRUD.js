7743138e407a5b3c8868e5c5edc60137c1764fff
Diego Vaca Paz <diegovacapaz@gmail.com>
2023-05-14 22:09:28 -0300

    Se hizo la modal para borrar categorias

--- js/controladorCategoriasCRUD.js
+++ js/controladorCategoriasCRUD.js
@@ -1,6 +1,8 @@
 import {categorias, crearCategoria, borrarCategoria} from "./modeloCategoriasCRUD.js";
 
 let btnNuevaCat = document.getElementById("btnNuevaCat");
+let tablaCat = document.getElementById("tablaCategorias");
+let botonesBorrarCat = document.getElementsByClassName("btn-borrarCat");
 
 btnNuevaCat.addEventListener("click", (e) => {
     e.preventDefault();
@@ -13,3 +15,34 @@ btnNuevaCat.addEventListener("click", (e) => {
     }
 });
 
+const actualizarTablaCat = () => {
+    tablaCat.tBodies[0].innerHTML = "";
+    let trCat;
+    let tdCat;
+    for(let i = 0; i < categorias.length; i++){
+        trCat = tablaCat.tBodies[0].insertRow();
+        tdCat = trCat.insertCell();
+        tdCat.innerText = categorias[i];
+        tdCat = trCat.insertCell();
+        tdCat.appendChild(crearBotonBorrarCat());
+        tdCat.setAttribute("class","text-center");
+    }
+    asignarListenersBorrar();
+}
+
+const asignarListenersBorrar = () => {
+    for(let i = 0; i < botonesBorrarCat.length; i++){
+        botonesBorrarCat[i].addEventListener("click", () => {
+            borrarCategoria(categorias[i]);
+            actualizarTablaCat();
+        });
+    }
+}
+
+const crearBotonBorrarCat = () => {
+    let borrar = document.createElement("i");
+    borrar.setAttribute("class","bi bi-x-circle-fill btn-borrarCat");
+    return borrar;
+}
+
+actualizarTablaCat();
