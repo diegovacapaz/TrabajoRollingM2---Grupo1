a71b10c985e3e0bbf524291829356c9f1073458e
Diego Vaca Paz <diegovacapaz@gmail.com>
2023-05-16 19:12:51 -0300

    Se creo un archivo soporte de log solo para index

---
+++ js/controladorBotonLogOutInIndex.js
@@ -0,0 +1,68 @@
+//IMPORTACIÓN DEL MODELO
+import {cuentaActiva, cerrarSesion} from "./modeloUsuariosCRUD.js";
+
+//Parametros globales
+const contenedorLog = document.getElementById("contenedorLog");
+const botonLog = document.getElementById("botonLog");
+const botonLogDivTexto = document.getElementById("botonLogTexto");
+
+//Funciones
+const actualizarCuentaActiva = ()=>{
+    if(cuentaActiva !== null){
+        botonLogDivTexto.innerHTML = cuentaActiva.usuario;
+        let iconLogout = document.createElement("button");
+        iconLogout.innerHTML = '<i class="bi bi-x-circle"></i>';
+        iconLogout.className ="btn btn-outline-light crucetaLogout";
+        iconLogout.addEventListener("click",()=>{
+            cerrarSesion();
+            window.location.reload();
+        });
+        contenedorLog.appendChild(iconLogout);
+    }
+    else{
+        botonLogDivTexto.innerHTML = "Ingresar";
+    }
+    añadirEventoClickBotonLog();
+}
+
+const añadirEventoClickBotonLog = ()=>{
+    if(cuentaActiva !== null){
+        botonLog.addEventListener("click",()=>{
+            window.location.href = "./pages/error404.html"; //Revisar la ruta en todos los html
+        });
+    }
+    else{
+        botonLog.addEventListener("click",()=>{
+            window.location.href = "./pages/LoginRegistro.html"; //Revisar la ruta en todos los html
+        });
+    }
+}
+
+//Inicialización
+actualizarCuentaActiva();
+
+
+
+//Testing
+
+// registro("diego","diego@","1234");
+// iniciarSesion("admin@admin.com","1234");
+// iniciarSesion("diego@","1234");
+
+
+
+
+
+/*Revisar estado de cuentaActiva
+
+Si hay una cuenta logueada,
+mostrar su nombre de usuario y agregar un boton de logout
+el usuario lleva a error404
+el logo lleva a error404
+el logout cierra sesion y recarga la pagina
+
+Si no hay cuenta logueada,
+mostrar ingresar
+el logo lleva a LoginRegistro.html
+el ingresar lleva a LoginRegistro.html
+*/
